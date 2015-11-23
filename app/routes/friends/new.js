c574844573bfa34572209ba82889fa2360692f88
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-22 19:04:37 -0500

    Implement controller reset for friend

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,22 @@ export default Ember.Route.extend({
   model(){
     return this.store.createRecord('friend');
   },
+
+  // activate(){
+  //   console.log('-----activate hook called-----')
+  // },
+  // deactivate(){
+  //   console.log('-----deactivate hook called-----')
+  // },
+
+  resetController: function(controller, isExisting, transition){
+    if (isExisting){
+      var model = controller.get('model');
+      if (model.get('isNew')){
+        model.destroyRecord();
+      }
+    }
+  },
   actions:{
     save(){
       console.log('+--save action bubbled up to friends new route');
